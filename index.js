72c47aaacf95fe076ff6f5cfcbc50bc11e8d1697
LpHiX <martin.leung.ht@gmail.com>
2022-08-09 21:38:02 +0800

    rewrite

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById("canvas");
-var context = canvas.getContext('2d');
+var context = canvas.getContext("2d");
 var fpsMeter = document.getElementById("fpsMeter");
 var debugText = document.getElementById("debugText");
 var Vec2 = /** @class */ (function () {
@@ -13,126 +13,112 @@ var Vec2 = /** @class */ (function () {
     Vec2.prototype.mul = function (scalar) {
         return new Vec2(this.x * scalar, this.y * scalar);
     };
-    Vec2.randomVec2 = function (xMin, yMin, xMax, yMax) {
-        return new Vec2(Math.random() * (xMax - xMin) + xMin, Math.random() * (yMax - yMin) + yMin);
-    };
     Vec2.prototype.toString = function () {
         return "vec2(".concat(this.x, ", ").concat(this.y, ")");
     };
     return Vec2;
 }());
-var Circle = /** @class */ (function () {
-    function Circle(pos, vel, radius) {
-        this.pos = pos;
-        this.vel = vel;
-        this.radius = radius;
-    }
-    Circle.prototype.draw = function () {
-        context.beginPath();
-        context.strokeStyle = "#0FFFFF";
-        context.arc(this.pos.x, this.pos.y, this.radius, 0, 2 * Math.PI);
-        context.stroke();
-    };
-    Circle.prototype.update = function () {
-        this.pos = this.pos.add(this.vel);
-        if (this.pos.x % (canvas.width - this.radius) < this.radius) {
-            this.vel.x *= -1;
-        }
-        if (this.pos.y % (canvas.height - this.radius) < this.radius) {
-            this.vel.y *= -1;
-        }
-    };
-    return Circle;
-}());
 var Grid = /** @class */ (function () {
-    function Grid(minX, minY, maxX, maxY) {
-        this.minX = minX;
-        this.minY = minY;
-        this.maxX = maxX;
-        this.maxY = maxY;
-        this.xRange = this.maxX - this.minX;
-        this.yRange = this.maxY - this.minY;
+    function Grid(size) {
+        this.size = size;
+        this.canvasMin = Math.min(canvas.width, canvas.height);
     }
     Grid.prototype.gridToCanvas = function (coord) {
-        var x = canvas.width / 2 + coord.x / this.xRange * 2 * canvas.width / 2;
-        var y = canvas.height / 2 - coord.y / this.yRange * 2 * canvas.height / 2;
+        var x = canvas.width / 2 + coord.x / this.size * this.canvasMin / 2;
+        var y = canvas.height / 2 - coord.y / this.size * this.canvasMin / 2;
         return new Vec2(x, y);
     };
     Grid.prototype.canvasToGrid = function (coord) {
-        var x = coord.x * 2 / canvas.width - 1;
-        var y = coord.y * 2 / canvas.height - 1;
-        return new Vec2(x * this.xRange / 2, -y * this.yRange / 2);
-    };
-    Grid.prototype.drawMajorGrid = function () {
-        context.strokeStyle = "#0FFFFF";
-        var xGridStart = this.gridToCanvas(new Vec2(this.minX, 0));
-        var xGridEnd = this.gridToCanvas(new Vec2(this.maxX, 0));
-        var yGridStart = this.gridToCanvas(new Vec2(0, this.minY));
-        var yGridEnd = this.gridToCanvas(new Vec2(0, this.maxY));
-        context.beginPath();
-        context.moveTo(xGridStart.x, xGridStart.y);
-        context.lineTo(xGridEnd.x, xGridEnd.y);
-        context.stroke();
-        context.beginPath();
-        context.moveTo(yGridStart.x, yGridStart.y);
-        context.lineTo(yGridEnd.x, yGridEnd.y);
-        context.stroke();
+        var x = coord.x * 2 - canvas.width;
+        var y = coord.y * 2 - canvas.height;
+        return new Vec2(x, -y).mul(this.size / this.canvasMin);
     };
     Grid.prototype.drawMinorGrid = function (gridSize) {
+        context.strokeStyle = "rgb(50, 100, 200)";
         for (var sign = -1; sign <= 1; sign += 2) {
-            for (var i = 1; i < canvas.width / 2 / gridSize; i++) {
-                var xGridStart = this.gridToCanvas(new Vec2(this.minX, i * gridSize * sign));
-                var xGridEnd = this.gridToCanvas(new Vec2(this.maxX, i * gridSize * sign));
-                var yGridStart = this.gridToCanvas(new Vec2(i * gridSize * sign, this.minY));
-                var yGridEnd = this.gridToCanvas(new Vec2(i * gridSize * sign, this.maxY));
-                context.strokeStyle = "#FFFFFF";
+            for (var i = 1; i < this.size / gridSize * canvas.height / this.canvasMin; i++) {
+                var MinX = this.gridToCanvas(new Vec2(-this.size * canvas.width / this.canvasMin, i * gridSize * sign));
+                var MaxX = this.gridToCanvas(new Vec2(this.size * canvas.width / this.canvasMin, i * gridSize * sign));
                 context.beginPath();
-                context.moveTo(xGridStart.x, xGridStart.y);
-                context.lineTo(xGridEnd.x, xGridEnd.y);
+                context.moveTo(MinX.x, MinX.y);
+                context.lineTo(MaxX.x, MaxX.y);
                 context.stroke();
+            }
+            for (var i = 1; i < this.size / gridSize * canvas.width / this.canvasMin; i++) {
+                var MinY = this.gridToCanvas(new Vec2(i * gridSize * sign, -this.size * canvas.height / this.canvasMin));
+                var MaxY = this.gridToCanvas(new Vec2(i * gridSize * sign, this.size * canvas.height / this.canvasMin));
                 context.beginPath();
-                context.moveTo(yGridStart.x, yGridStart.y);
-                context.lineTo(yGridEnd.x, yGridEnd.y);
+                context.moveTo(MinY.x, MinY.y);
+                context.lineTo(MaxY.x, MaxY.y);
                 context.stroke();
             }
         }
+    };
+    Grid.prototype.drawMajorGrid = function () {
+        var MinX = this.gridToCanvas(new Vec2(-this.size * canvas.width / this.canvasMin, 0));
+        var MaxX = this.gridToCanvas(new Vec2(this.size * canvas.width / this.canvasMin, 0));
+        var MinY = this.gridToCanvas(new Vec2(0, -this.size * canvas.height / this.canvasMin));
+        var MaxY = this.gridToCanvas(new Vec2(0, this.size * canvas.height / this.canvasMin));
         context.beginPath();
-        context.moveTo(this.minX, this.minY);
+        context.strokeStyle = "rgb(200, 200, 200)";
+        context.moveTo(MinX.x, MinX.y);
+        context.lineTo(MaxX.x, MaxX.y);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(MinY.x, MinY.y);
+        context.lineTo(MaxY.x, MaxY.y);
+        context.stroke();
     };
     return Grid;
 }());
-var circles = [];
-var mouseX, mouseY, grid, previousFrameTime;
-function canvasClicked(event) {
-    var rect = canvas.getBoundingClientRect();
-    mouseX = event.clientX - rect.left;
-    mouseY = event.clientY - rect.top;
-    circles.push(new Circle(new Vec2(mouseX, mouseY), Vec2.randomVec2(-1, -1, 1, 1), 140));
-    debugText.innerText =
-        new Vec2(mouseX, mouseY).toString() + " Original canvas coordinates \n" +
-            grid.canvasToGrid(new Vec2(mouseX, mouseY)).toString() + " Grid coordinates \n" +
-            grid.gridToCanvas(grid.canvasToGrid(new Vec2(mouseX, mouseY))).toString() + " Calculated canvas coordinates";
+function funcValue(x, y) {
+    return (y - x * x / 2) + 15;
 }
-function setup() {
-    grid = new Grid(-10, -10, 10, 10);
-    for (var i = 0; i < 10; i++) {
-        circles.push(new Circle(Vec2.randomVec2(100, 100, canvas.width - 100, canvas.height - 100), Vec2.randomVec2(-1, -1, 1, 1), 10));
+var SubGrid = /** @class */ (function () {
+    function SubGrid(gridSize) {
+        this.gridSize = gridSize;
     }
-    canvas.addEventListener("click", canvasClicked);
+    SubGrid.prototype.forEachCorner = function (func) {
+        var maxXIndex = Math.floor(grid.size / this.gridSize * canvas.width / grid.canvasMin);
+        var maxYIndex = Math.floor(grid.size / this.gridSize * canvas.height / grid.canvasMin);
+        for (var y = -maxYIndex; y <= maxYIndex; y++) {
+            for (var x = -maxXIndex; x <= maxXIndex; x++) {
+                func(x * this.gridSize, y * this.gridSize);
+            }
+        }
+    };
+    SubGrid.prototype.showValue = function (x, y) {
+        var val = funcValue(x, y);
+        var coord = grid.gridToCanvas(new Vec2(x, y));
+        context.strokeStyle = "rgb(0, ".concat(val >= 0 ? 255 : 0, ", 0)");
+        context.beginPath();
+        context.arc(coord.x, coord.y, 3, 0, 2 * Math.PI);
+        context.stroke();
+    };
+    return SubGrid;
+}());
+function canvasClick(event) {
+    var rect = canvas.getBoundingClientRect();
+    var clickedCoord = new Vec2(event.clientX - rect.left, event.clientY - rect.top);
+    var toGridCoord = grid.canvasToGrid(clickedCoord);
+    var toCanvasCoord = grid.gridToCanvas(toGridCoord);
+    debugText.innerText =
+        "\n    ".concat(clickedCoord, " Original \n    ").concat(toGridCoord, " To Grid\n    ").concat(toCanvasCoord, " To Canvas\n    ");
+}
+var lastFrameTime, grid, subGrid;
+function setup() {
+    grid = new Grid(10);
+    subGrid = new SubGrid(1);
+    canvas.addEventListener("mousedown", canvasClick);
+    context.fillStyle = "rgb(30,40,50)";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    grid.drawMajorGrid();
+    grid.drawMinorGrid(2);
+    subGrid.forEachCorner(subGrid.showValue);
 }
 function frameUpdate(timestamp) {
-    fpsMeter.textContent = (1 / ((timestamp - previousFrameTime) / 1000)).toString();
-    previousFrameTime = timestamp;
-    context.fillStyle = "rgb(0,0,0)";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    var imageData = context.createImageData(canvas.width, canvas.height);
-    var data = imageData.data;
-    grid.drawMajorGrid();
-    grid.drawMinorGrid(1);
-    circles.forEach(function (element) {
-        element.draw();
-        element.update();
-    });
+    fpsMeter.innerText = (1 / ((timestamp - lastFrameTime) / 1000)).toString();
+    lastFrameTime = timestamp;
     window.requestAnimationFrame(frameUpdate);
 }
 setup();
--- index.ts
+++ index.ts
@@ -1,161 +1,143 @@
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-const context = canvas.getContext('2d') as CanvasRenderingContext2D;
-
+const context = canvas.getContext("2d") as CanvasRenderingContext2D;
 const fpsMeter = document.getElementById("fpsMeter") as HTMLDivElement;
 const debugText = document.getElementById("debugText") as HTMLDivElement;
-
+ 
 class Vec2{
-    x:number;
-    y:number;
-    constructor(x:number, y:number){
+    x: number;
+    y: number;
+    constructor(x:number, y:number)
+    {
         this.x = x;
         this.y = y;
     }
     add(other: Vec2):Vec2{
         return new Vec2(this.x + other.x, this.y + other.y);
     }
-    mul(scalar:number):Vec2{
+    mul(scalar: number):Vec2{
         return new Vec2(this.x * scalar, this.y * scalar);
     }
-    static randomVec2(xMin, yMin, xMax, yMax):Vec2{
-       return new Vec2(Math.random() * (xMax - xMin) + xMin, Math.random() * (yMax - yMin) + yMin);
-    }
-    toString(): string{
+    toString():string{
         return `vec2(${this.x}, ${this.y})`;
     }
 }
-class Circle{
-    pos:Vec2;
-    vel:Vec2;
-    radius:number;
-    constructor(pos:Vec2, vel:Vec2, radius){
-        this.pos = pos;
-        this.vel = vel;
-        this.radius = radius;
-    }
-    draw(): void{
-        context.beginPath();
-        context.strokeStyle = "#0FFFFF"
-        context.arc(this.pos.x, this.pos.y, this.radius, 0, 2 * Math.PI);
-        context.stroke();
-    }
-    update(): void{
-        this.pos = this.pos.add(this.vel);
-        if(this.pos.x % (canvas.width - this.radius) < this.radius){
-            this.vel.x *= -1;
-        }
-        if(this.pos.y % (canvas.height - this.radius) < this.radius){
-            this.vel.y *= -1;
-        }
-    }
-}
 class Grid{
-    minX: number;
-    minY: number;
-    maxX: number;
-    maxY: number;
-    xRange: number;
-    yRange: number;
-    constructor(minX:number, minY:number, maxX:number, maxY:number){
-        this.minX = minX;
-        this.minY = minY;
-        this.maxX = maxX;
-        this.maxY = maxY;
-        this.xRange = this.maxX - this.minX;
-        this.yRange = this.maxY - this.minY;
+    size: number;
+    canvasMin: number;
+    constructor(size:number){
+        this.size = size;
+        this.canvasMin = Math.min(canvas.width, canvas.height);
     }
-    gridToCanvas(coord:Vec2):Vec2 {
-        let x = canvas.width / 2 + coord.x / this.xRange * 2 * canvas.width / 2;
-        let y = canvas.height / 2 - coord.y / this.yRange * 2 * canvas.height /2;
+    gridToCanvas(coord: Vec2): Vec2{
+        let x = canvas.width / 2 + coord.x / this.size * this.canvasMin / 2;
+        let y = canvas.height / 2 - coord.y / this.size * this.canvasMin / 2;
         return new Vec2(x,y);
     }
-    canvasToGrid(coord:Vec2):Vec2 {
-        let x = coord.x * 2 / canvas.width - 1;
-        let y = coord.y * 2 / canvas.height - 1;
-        return new Vec2(x * this.xRange / 2,-y * this.yRange / 2);
+    canvasToGrid(coord: Vec2): Vec2{
+        var x = coord.x * 2 - canvas.width;
+        var y = coord.y * 2 - canvas.height;
+        return new Vec2(x,-y).mul(this.size / this.canvasMin);
     }
-    drawMajorGrid(): void{
-        context.strokeStyle = "#0FFFFF"
-        const xGridStart = this.gridToCanvas(new Vec2(this.minX, 0));
-        const xGridEnd = this.gridToCanvas(new Vec2(this.maxX, 0));
-        const yGridStart = this.gridToCanvas(new Vec2(0, this.minY));
-        const yGridEnd = this.gridToCanvas(new Vec2(0, this.maxY));
-        context.beginPath();
-        context.moveTo(xGridStart.x, xGridStart.y)
-        context.lineTo(xGridEnd.x, xGridEnd.y)
+    drawMinorGrid(gridSize:number) {
+        context.strokeStyle = "rgb(50, 100, 200)";
+        for(var sign = -1; sign <= 1; sign += 2) {
+            for (let i = 1; i < this.size / gridSize * canvas.height / this.canvasMin; i++) {
+                let MinX = this.gridToCanvas(new Vec2(-this.size * canvas.width / this.canvasMin, i * gridSize * sign))
+                let MaxX = this.gridToCanvas(new Vec2(this.size * canvas.width / this.canvasMin, i * gridSize * sign))
+ 
+                context.beginPath()
+                context.moveTo(MinX.x, MinX.y);
+                context.lineTo(MaxX.x, MaxX.y);
+                context.stroke();
+                }
+                for (let i = 1; i < this.size / gridSize * canvas.width / this.canvasMin; i++) {
+                    let MinY = this.gridToCanvas(new Vec2(i * gridSize * sign, -this.size * canvas.height / this.canvasMin))
+                    let MaxY = this.gridToCanvas(new Vec2(i * gridSize * sign, this.size * canvas.height / this.canvasMin))
+ 
+                    context.beginPath()
+                    context.moveTo(MinY.x, MinY.y);
+                    context.lineTo(MaxY.x, MaxY.y);
+                    context.stroke();
+                }   
+            }
+    }
+    drawMajorGrid() {
+        let MinX = this.gridToCanvas(new Vec2(-this.size * canvas.width / this.canvasMin, 0))
+        let MaxX = this.gridToCanvas(new Vec2(this.size * canvas.width / this.canvasMin, 0))
+        let MinY = this.gridToCanvas(new Vec2(0, -this.size * canvas.height / this.canvasMin))
+        let MaxY = this.gridToCanvas(new Vec2(0, this.size * canvas.height / this.canvasMin))
+ 
+        context.beginPath()
+        context.strokeStyle = "rgb(200, 200, 200)";
+        context.moveTo(MinX.x, MinX.y);
+        context.lineTo(MaxX.x, MaxX.y);
         context.stroke();
-
-        context.beginPath();
-        context.moveTo(yGridStart.x, yGridStart.y)
-        context.lineTo(yGridEnd.x, yGridEnd.y)
+ 
+        context.beginPath()
+        context.moveTo(MinY.x, MinY.y);
+        context.lineTo(MaxY.x, MaxY.y);
         context.stroke();
     }
-    drawMinorGrid(gridSize:number): void{
-        for (let sign = -1; sign <= 1; sign += 2){
-            for (let i = 1; i < canvas.width / 2 / gridSize; i++) {
-                const xGridStart = this.gridToCanvas(new Vec2(this.minX, i * gridSize * sign));
-                const xGridEnd = this.gridToCanvas(new Vec2(this.maxX, i * gridSize * sign));
-                const yGridStart = this.gridToCanvas(new Vec2(i * gridSize * sign, this.minY));
-                const yGridEnd = this.gridToCanvas(new Vec2(i * gridSize * sign, this.maxY));
-                context.strokeStyle = "#FFFFFF"
-                context.beginPath();
-                context.moveTo(xGridStart.x, xGridStart.y); 
-                context.lineTo(xGridEnd.x, xGridEnd.y)
-                context.stroke();
-
-                context.beginPath();
-                context.moveTo(yGridStart.x, yGridStart.y)
-                context.lineTo(yGridEnd.x, yGridEnd.y)
-                context.stroke();
+}
+function funcValue(x: number, y: number):number{
+    return (y - x * x / 2) + 15;
+}
+ 
+class SubGrid{
+    gridSize:number;
+    constructor(gridSize: number){
+        this.gridSize = gridSize;
+    }
+    forEachCorner(func: Function){
+        let maxXIndex = Math.floor(grid.size / this.gridSize * canvas.width / grid.canvasMin);
+        let maxYIndex = Math.floor(grid.size / this.gridSize * canvas.height / grid.canvasMin);
+        for(let y = -maxYIndex; y <= maxYIndex; y++){
+            for(let x = -maxXIndex; x <= maxXIndex; x++){
+                func(x*this.gridSize,y*this.gridSize);
             }
         }
+    }
+    showValue(x:number,y:number): void{
+        let val = funcValue(x,y);
+        let coord = grid.gridToCanvas(new Vec2(x,y));
+        context.strokeStyle = `rgb(0, ${val >= 0 ? 255 : 0}, 0)`
         context.beginPath();
-        context.moveTo(this.minX, this.minY);
+        context.arc(coord.x, coord.y, 3, 0, 2 * Math.PI);
+        context.stroke();
     }
 }
-
-var circles: Circle[] = [];
-var mouseX:number, mouseY:number, grid:Grid, previousFrameTime:number;
-
-function canvasClicked(event:MouseEvent){
-    var rect = canvas.getBoundingClientRect();
-    mouseX = event.clientX - rect.left;
-    mouseY = event.clientY - rect.top;
-    circles.push(new Circle(new Vec2(mouseX, mouseY), Vec2.randomVec2(-1, -1, 1, 1), 140))
+function canvasClick(event: MouseEvent): void {
+    const rect = canvas.getBoundingClientRect();
+    const clickedCoord = new Vec2(event.clientX - rect.left, event.clientY - rect.top);
+    const toGridCoord = grid.canvasToGrid(clickedCoord);
+    const toCanvasCoord = grid.gridToCanvas(toGridCoord);
     debugText.innerText = 
-    new Vec2(mouseX, mouseY).toString() + " Original canvas coordinates \n" +
-    grid.canvasToGrid(new Vec2(mouseX, mouseY)).toString() + " Grid coordinates \n" +
-    grid.gridToCanvas(grid.canvasToGrid(new Vec2(mouseX, mouseY))).toString() + " Calculated canvas coordinates";
+    `
+    ${clickedCoord} Original 
+    ${toGridCoord} To Grid
+    ${toCanvasCoord} To Canvas
+    `;
 }
-
-
+ 
+var lastFrameTime: number, grid: Grid, subGrid:SubGrid;
+ 
 function setup(){
-    grid = new Grid(-10, -10, 10, 10);
-    for (let i = 0; i < 10; i++) {
-        circles.push(new Circle(Vec2.randomVec2(100, 100, canvas.width-100, canvas.height-100), Vec2.randomVec2(-1, -1, 1, 1), 10));
-    }
-    canvas.addEventListener("click", canvasClicked);
-}
-
-function frameUpdate(timestamp){
-    fpsMeter.textContent = (1 / ((timestamp - previousFrameTime)/ 1000)).toString();
-    previousFrameTime = timestamp;
-
-    context.fillStyle = "rgb(0,0,0)";
+    grid = new Grid(10);
+    subGrid = new SubGrid(1);
+    canvas.addEventListener("mousedown", canvasClick);
+ 
+    context.fillStyle = "rgb(30,40,50)";
     context.fillRect(0, 0, canvas.width, canvas.height);
-
-    var imageData = context.createImageData(canvas.width, canvas.height);
-    var data = imageData.data;
-
     grid.drawMajorGrid();
-    grid.drawMinorGrid(1);
-
-    circles.forEach(element => {
-        element.draw();
-        element.update();
-    });
+    grid.drawMinorGrid(2);
+ 
+    subGrid.forEachCorner(subGrid.showValue)
+}
+function frameUpdate(timestamp: number){
+    fpsMeter.innerText = (1 / ((timestamp - lastFrameTime) / 1000)).toString();
+    lastFrameTime = timestamp;
+ 
     window.requestAnimationFrame(frameUpdate);
 }
-
 setup();
 window.requestAnimationFrame(frameUpdate);
