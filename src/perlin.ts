da3d79ace213e0fde1e9565ca5c19e4d54dc488d
LpHiX <martin.leung.ht@gmail.com>
2022-08-10 23:42:23 +0800

    fixed perlin

--- src/perlin.ts
+++ src/perlin.ts
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const canvas = document.getElementById("canvas2") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 class Vec2{
@@ -16,12 +16,12 @@ class Vec2{
         return this.x * other.x + this.y * other.y;
     }
     static randomVec(){
-        return new Vec2(Math.random() * 2 - 1, Math.random() * 2 - 1)
+        return new Vec2(Math.random() * 2 - 1, Math.random() * 2 - 1);
     }
 }
 
-function lerp(alpha:number, a:number, b:number): number {
-    return a + (b - a) * alpha;
+function lerp(w:number, a:number, b:number): number {
+    return a + (b - a) * (w * w * w * ((6 * w - 15) * w + 10));
 }
 
 class PerlinLayer{
@@ -29,9 +29,9 @@ class PerlinLayer{
     constructor(
         public size:number
     ){
-        for(var x = 0; x < size; x++){
+        for(var x = 0; x <= size; x++){
             this.corners.push([]);
-            for(var y = 0; y < size; y++){
+            for(var y = 0; y <= size; y++){
                 this.corners[x].push(Vec2.randomVec());
             }
         }
@@ -40,13 +40,13 @@ class PerlinLayer{
         var normVec = new Vec2(canvasX / canvas.width * this.size, canvasY / canvas.height * this.size);
         var indexVec = new Vec2(Math.floor(normVec.x), Math.floor(normVec.y));
         var fracVec = new Vec2(normVec.x % 1, normVec.y % 1);
-        var dot0 = fracVec.dot(this.corners[indexVec.x][indexVec.y]);
-        var dot1 = fracVec.dot(this.corners[indexVec.x + 1][indexVec.y]);
-        var dot2 = fracVec.dot(this.corners[indexVec.x][indexVec.y + 1]);
-        var dot3 = fracVec.dot(this.corners[indexVec.x + 1][indexVec.y + 1]);
+        var dot0 = new Vec2(fracVec.x, fracVec.y).dot(this.corners[indexVec.x][indexVec.y]);
+        var dot1 = new Vec2(fracVec.x - 1, fracVec.y).dot(this.corners[indexVec.x + 1][indexVec.y]);
+        var dot2 = new Vec2(fracVec.x, fracVec.y - 1).dot(this.corners[indexVec.x][indexVec.y + 1]);
+        var dot3 = new Vec2(fracVec.x - 1, fracVec.y - 1).dot(this.corners[indexVec.x + 1][indexVec.y + 1]);
         var x0 = lerp(fracVec.x, dot0, dot1);
         var x1 = lerp(fracVec.x, dot2, dot3);
-        return lerp(fracVec.y, x0, x1;)
+        return lerp(fracVec.y, x0, x1);
     }
 }
 
@@ -55,6 +55,16 @@ function setup(){
     var id = ctx.createImageData(canvas.width, canvas.height);
     var d = id.data;
     var layer0 = new PerlinLayer(4);
-    
+    for(var x = 0; x < canvas.width; x++){
+        for(var y = 0; y < canvas.height; y++){
+            d[(y * canvas.width + x) * 4] = 0;
+            d[(y * canvas.width + x) * 4 + 1] = layer0.value(x,y) * 255;
+            d[(y * canvas.width + x) * 4 + 2] = 0;
+            d[(y * canvas.width + x) * 4 + 3] = 255;
+        }
+    }
+    ctx.putImageData(id, 0, 0);
+    ctx.fillStyle = "rgb(20, 40, 50)";
+    ctx.fillRect(0, 0, 20, 30);
 }
 setup();
