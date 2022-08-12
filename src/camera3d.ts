e27dec0ca2f2a24bd28c1c3632b75a54b9b52556
LpHiX <martin.leung.ht@gmail.com>
2022-08-13 02:30:36 +0800

    render objects, TODO: Z sorting

--- src/camera3d.ts
+++ src/camera3d.ts
@@ -28,7 +28,7 @@ class Vec3{
         return new Vec3(this.x * scalar, this.y * scalar, this.z * scalar);
     }
     static lerp(alpha:number, a:Vec3, b:Vec3){
-        return a.add(b.add(a.mul(-1)).mul(alpha);
+        return a.add(b.add(a.mul(-1)).mul(alpha));
     }
 }
 class Camera{
@@ -41,7 +41,8 @@ class Camera{
 class Util{
     canvasMin:number;
     constructor(
-        public size:number
+        public size:number,
+        public camera:Camera
     ){
         this.canvasMin = Math.min(canvas.width, canvas.height);
     }
@@ -51,8 +52,12 @@ class Util{
     g2c(coord:Vec2):Vec2{
         return new Vec3(canvas.width / 2 + coord.x / this.size * canvas.width / 2, canvas.height / 2 - coord.y / this.size * canvas.height / 2, 0)
     }
-    vecToGrid(coord: Vec3):Vec2{
-        return new Vec2(coord.x / (coord.z), coord.y / (coord.z));
+    vecToGrid(coord: Vec3):Vec2 | null{
+        if(coord.z > 0){
+            return new Vec2(coord.x / (coord.z), coord.y / (coord.z));
+        } else {
+            return null;
+        }
     }
     rotZ(coord:Vec3, angle:number):Vec3{
         return new Vec3(coord.x * Math.cos(angle) - coord.y * Math.sin(angle), coord.x * Math.sin(angle) + coord.y * Math.cos(angle), coord.z);
@@ -63,66 +68,93 @@ class Util{
     rotY(coord:Vec3, angle:number):Vec3{
         return new Vec3(coord.x * Math.cos(angle) - coord.z * Math.sin(angle), coord.y, coord.x * Math.sin(angle) + coord.z * Math.cos(angle));
     }
-    vecToCanvas(coord:Vec3):Vec2{
-        return this.g2c(this.vecToGrid(util.rotX(util.rotY(coord, cam.angleY), cam.angleX)));
+    vecToCanvas(coord:Vec3):Vec2 | null{
+        const tempPoint = this.vecToGrid(util.rotX(util.rotY(coord.add(this.camera.pos.mul(-1)), cam.angleY), cam.angleX))
+        if(tempPoint !== null){
+            return this.g2c(tempPoint);
+        } else {
+            return null;
+        }
     }
 }
-let down = false;
-let startX = 0;
-let startY = 0;
-let premoveY = 0;
-let premoveX = 0;
 abstract class object3d{
-    verticies: Vec3[],
-    triangles: number[],
-    fillStyle: string
-}
-class Cube implements object3d{
-    readonly verticies: Vec3[];
-    readonly triangles: number[];
-    readonly planes: Plane[] = [];
     constructor(
-        readonly pos:Vec3,
-        public scale: Vec3,
+        public verticies: Vec3[],
+        public triangles: number[],
+        public pos: Vec3,
         public fillStyle: string
-    ){
-        this.verticies = [
-            new Vec3(-0.5 * scale.x, -0.5 * scale.y, -0.5*scale.z).add(pos),
-            new Vec3( 0.5 * scale.x, -0.5 * scale.y, -0.5*scale.z).add(pos),
-            new Vec3(-0.5 * scale.x,  0.5 * scale.y, -0.5*scale.z).add(pos),
-            new Vec3( 0.5 * scale.x,  0.5 * scale.y, -0.5*scale.z).add(pos),
-            new Vec3(-0.5 * scale.x, -0.5 * scale.y,  0.5*scale.z).add(pos),
-            new Vec3( 0.5 * scale.x, -0.5 * scale.y,  0.5*scale.z).add(pos),
-            new Vec3(-0.5 * scale.x,  0.5 * scale.y,  0.5*scale.z).add(pos),
-            new Vec3( 0.5 * scale.x,  0.5 * scale.y,  0.5*scale.z).add(pos)
-        ];
-        this.triangles = [
-            0,1,3,
-            0,2,3,
-            4,5,7,
-            4,6,7,
-            0,4,6,
-            0,2,6,
-            1,3,7,
-            1,5,7,
-            0,1,5,
-            0,4,5,
-            2,3,7,
-            2,6,7
-        ];
+    ){}
+    draw(): void{
+        ctx.fillStyle = this.fillStyle;
+    
+        for(var i = 0; i < this.triangles.length / 3; i++){
+            const corner0 = util.vecToCanvas(this.verticies[this.triangles[i* 3]].add(this.pos));
+            const corner1 = util.vecToCanvas(this.verticies[this.triangles[i* 3 + 1]].add(this.pos));
+            const corner2 = util.vecToCanvas(this.verticies[this.triangles[i* 3 + 2]].add(this.pos));
+            if(corner0 !== null && corner1 !== null && corner2 !== null){
+                ctx.beginPath();
+                ctx.moveTo(corner0.x, corner0.y);
+                ctx.lineTo(corner1.x, corner1.y);
+                ctx.lineTo(corner2.x, corner2.y);
+                ctx.closePath()
+                ctx.stroke();
+                ctx.fill();
+            }
+        }
+    }
+    rotateY(angle: number): void {
+        for(var i = 0; i < this.verticies.length; i++){
+            this.verticies[i] = util.rotY(this.verticies[i], angle);
+        }
     }
 }
-class Plane implements object3d{
-    verticies: Vec3[] = [];
-    triangles: number[] = [];
+class Cube extends object3d{
     constructor(
-        public corners: Vec3[],
-        public size: number,
+        public scale: Vec3,
+        public pos: Vec3,
         public fillStyle: string
     ){
-        for(var y = 0; y <= size; y++){
+        super(
+            [ //Verticies
+                new Vec3(-0.5 * scale.x, -0.5 * scale.y, -0.5*scale.z),
+                new Vec3( 0.5 * scale.x, -0.5 * scale.y, -0.5*scale.z),
+                new Vec3(-0.5 * scale.x,  0.5 * scale.y, -0.5*scale.z),
+                new Vec3( 0.5 * scale.x,  0.5 * scale.y, -0.5*scale.z),
+                new Vec3(-0.5 * scale.x, -0.5 * scale.y,  0.5*scale.z),
+                new Vec3( 0.5 * scale.x, -0.5 * scale.y,  0.5*scale.z),
+                new Vec3(-0.5 * scale.x,  0.5 * scale.y,  0.5*scale.z),
+                new Vec3( 0.5 * scale.x,  0.5 * scale.y,  0.5*scale.z)
+            ], [ //Triangles
+                0,1,3,
+                0,2,3,
+                4,5,7,
+                4,6,7,
+                0,4,6,
+                0,2,6,
+                1,3,7,
+                1,5,7,
+                0,1,5,
+                0,4,5,
+                2,3,7,
+                2,6,7
+            ], pos, fillStyle);
+    }
+}
+class Plane extends object3d{
+    constructor(
+        public size: number,
+        public scale: Vec3,
+        public pos: Vec3,
+        public fillStyle: string
+    ){
+        super(
+            [], [], pos, fillStyle
+        );
+        for(var z = 0; z <= size; z++){
             for(var x = 0; x <= size; x++){
-                const newPoint = Vec3.lerp(y / size, Vec3.lerp(x / size, corners[0], corners[1]), Vec3.lerp(x / size, corners[2], corners[3]));
+                const newPoint = Vec3.lerp(z / size, 
+                Vec3.lerp(x / size, new Vec3(-scale.x / 2, 0, -scale.z / 2), new Vec3(scale.x / 2, 0, -scale.z / 2)), 
+                Vec3.lerp(x / size, new Vec3(-scale.x / 2, 0, scale.z / 2), new Vec3(scale.x / 2, 0, scale.z / 2)));
                 this.verticies.push(newPoint);
             }
         }
@@ -138,32 +170,21 @@ class Plane implements object3d{
         }
     }
 }
-const cube=[
-    new Vec3(-1 + 3, -1 + 3, 5),
-    new Vec3( 1 + 3, -1 + 3, 5),
-    new Vec3( 1 + 3,  1 + 3, 5),
-    new Vec3(-1 + 3,  1 + 3, 5),
-    new Vec3(-1 + 3, -1 + 3, 3),
-    new Vec3( 1 + 3, -1 + 3, 3),
-    new Vec3( 1 + 3,  1 + 3, 3),
-    new Vec3(-1 + 3,  1 + 3, 3)
-];
-const indicies = [
-    0,1,2,
-    0,3,2,
-    0,1,5,
-    0,4,5,
-    0,3,7,
-    0,4,7,
-    2,3,7,
-    2,6,7,
-    1,2,6,
-    1,5,6,
-    4,5,6,
-    4,7,6
-]
-const util = new Util(4);
-const cam = new Camera(new Vec3(0,0,-10), 0, 0);
+
+
+const cam = new Camera(new Vec3(0,0,0), 0, 0);
+const util = new Util(1, cam);
+
+let down = false;
+let startX = 0;
+let startY = 0;
+let premoveY = 0;
+let premoveX = 0;
+let wPressed = false;
+let aPressed = false;
+let sPressed = false;
+let dPressed = false;
+
 function setup(){
     ctx.font = "10px Arial";
     canvas.addEventListener("mousedown", function(event) {
@@ -193,48 +214,77 @@ function setup(){
     canvas.addEventListener("mousemove", function(event) {
         if(down){
             cam.angleY = premoveY + (event.clientX - startX) / 100;
-            cam.angleX = premoveX + (event.clientY - startY) / 100;
+            cam.angleX = premoveX - (event.clientY - startY) / 100;
         }
     });
     canvas.addEventListener("touchmove", function(event) {
         if(down){
             cam.angleY = premoveY + (event.touches[0].clientX - startX) / 100;
-            cam.angleX = premoveX + (event.touches[0].clientY - startY) / 100;
+            cam.angleX = premoveX - (event.touches[0].clientY - startY) / 100;
         }
     });
+    document.addEventListener("keydown", function(event) {
+        switch(event.key){
+            case "w":
+                wPressed = true;
+                break;
+            case "a":
+                aPressed = true;
+                break;
+            case "s":
+                sPressed = true;
+                break;
+            case "d":
+                dPressed = true;
+                break;
+            default:
+                break;
+        }
+        //event.preventDefault();
+    });
+    document.addEventListener("keyup", function(event) {
+        switch(event.key){
+            case "w":
+                wPressed = false;
+                break;
+            case "a":
+                aPressed = false;
+                break;
+            case "s":
+                sPressed = false;
+                break;
+            case "d":
+                dPressed = false;
+                break;
+            default:
+                break;
+        }
+        //event.preventDefault();
+    });
 }
+const cube = new Cube(new Vec3(1, 1, 1), new Vec3( 2, 3, 5), "rgb(0, 255, 255)");
+const cube2 = new Cube(new Vec3(2, 3, 2), new Vec3( -3, 1, 4), "rgb(0, 255, 0)");
+const plane = new Plane(10, new Vec3(10, 1, 10), new Vec3(0, -1, 3), "rgb(255, 100, 200");
 function frameUpdate(timestamp:number){
     ctx.fillStyle = "rgb(30, 40, 50)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.strokeStyle = "rgb(0, 255, 255)";
-    ctx.fillStyle = "rgba(0, 255 , 0, 50)";
-
-    for(var i = 0; i < indicies.length / 3; i++){
-        ctx.beginPath();
-        const corner0 = util.vecToCanvas(cube[indicies[i* 3]]);
-        const corner1 = util.vecToCanvas(cube[indicies[i* 3 + 1]]);
-        const corner2 = util.vecToCanvas(cube[indicies[i* 3 + 2]]);
-        ctx.moveTo(corner0.x, corner0.y);
-        ctx.lineTo(corner1.x, corner1.y);
-        ctx.lineTo(corner2.x, corner2.y);
-        ctx.closePath()
-        ctx.stroke();
-        ctx.fill();
-
+    if(wPressed){
+        cam.pos = cam.pos.add(util.rotY(new Vec3(0, 0, 0.1), -cam.angleY));
     }
-
-    ctx.strokeStyle = "rgb(0, 255, 100)";
-    ctx.fillStyle = "rgb(255, 255 ,255)";
-    ctx.lineWidth = 1;
-    for(var i = 0; i < cube.length; i++){
-        const tempPoint = util.rotX(util.rotY(cube[i], cam.angleY), cam.angleX);
-        ctx.beginPath();
-        const point2d = util.g2c(util.vecToGrid(tempPoint));
-        ctx.arc(point2d.x, point2d.y, 1, 0, Math.PI * 2)
-        ctx.stroke()
-        ctx.fillText(i.toString(), point2d.x, point2d.y)
+    if(aPressed){
+        cam.pos = cam.pos.add(util.rotY(new Vec3(-0.1, 0, 0), -cam.angleY));
     }
+    if(sPressed){
+        cam.pos = cam.pos.add(util.rotY(new Vec3(0, 0, -0.1), -cam.angleY));
+    }
+    if(dPressed){
+        cam.pos = cam.pos.add(util.rotY(new Vec3(0.1, 0, 0), -cam.angleY));
+    }
+    cube.draw();
+    cube2.draw();
+    cube2.rotateY(0.1);
+    plane.draw();
 
     ctx.beginPath();
     const zero = util.g2c(new Vec3(0,0,0));
