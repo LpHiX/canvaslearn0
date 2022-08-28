eb2ba68326bae08b8b6b38c7a6b5d33a650f714f
LpHiX <erpastian@gmail.com>
2022-08-28 21:16:07 +0800

    up and down triangle

--- scripts/debug.js
+++ scripts/debug.js
@@ -6,12 +6,15 @@ const testImg = document.getElementById("testImg");
 const debugText = document.getElementById("debugText");
 var imageData = context.createImageData(canvas.width, canvas.height);
 var data = imageData.data;
-function changePixel(x, y, red, green, blue, alpha) {
-    data[(y * imageData.width + x) * 4] = red;
-    data[(y * imageData.width + x) * 4 + 1] = green;
-    data[(y * imageData.width + x) * 4 + 2] = blue;
-    data[(y * imageData.width + x) * 4 + 3] = alpha;
-}
+var test = document.createElement("canvas");
+test.width = testImg.naturalWidth;
+test.height = testImg.naturalHeight;
+//document.body.appendChild(test);
+test.style.borderStyle = "solid";
+test.style.borderWidth = "1px";
+var cont = test.getContext("2d");
+cont.drawImage(testImg, 0, 0);
+const testImageData = cont.getImageData(0, 0, testImg.naturalWidth, testImg.naturalHeight);
 class Vec2 {
     constructor(x, y) {
         this.x = x;
@@ -20,6 +23,15 @@ class Vec2 {
     toString() {
         return `Vec2(x: ${this.x}, y: ${this.y})`;
     }
+}
+const tVec0 = new Vec2(50, 800);
+const tVec1 = new Vec2(200, 500);
+const tVec2 = new Vec2(950, 50);
+function changePixel(x, y, red, green, blue, alpha) {
+    data[(y * imageData.width + x) * 4] = red;
+    data[(y * imageData.width + x) * 4 + 1] = green;
+    data[(y * imageData.width + x) * 4 + 2] = blue;
+    data[(y * imageData.width + x) * 4 + 3] = alpha;
 }
 function drawLine(verts) {
     var slope = (verts[1].y - verts[0].y) / (verts[1].x - verts[0].x);
@@ -76,41 +88,27 @@ function drawTriangle(verts) {
         delta2 += slope2;
     }
 }
-const tVec0 = new Vec2(50, 800);
-const tVec1 = new Vec2(700, 950);
-const tVec2 = new Vec2(950, 50);
 function getUV(x0, y0, vert0, vert1, vert2, down) {
     // vert0 - vert1 = a
     // vert1 - vert2 = b
     // inverse matrix to find a and b in terms of x and y
+    const determinant = (vert1.x - vert0.x) * (vert2.y - vert1.y) - (vert2.x - vert1.x) * (vert1.y - vert0.y);
+    const x = x0 - vert0.x;
+    const y = y0 - vert0.y;
     if (down) {
-        const determinant = (vert1.x - vert0.x) * (vert2.y - vert1.y) - (vert2.x - vert1.x) * (vert1.y - vert0.y);
-        const x = x0 - vert0.x;
-        const y = y0 - vert0.y;
-        return new Vec2(1 / determinant * (x * (vert2.y - vert1.y) + y * (vert1.x - vert2.x)), 1 / determinant * (x * (vert0.y - vert1.y) + y * (vert1.x - vert0.x)));
+        return new Vec2(1 / determinant * (x * (vert2.y - vert1.y) + y * (vert1.x - vert2.x)), 1 - 1 / determinant * (x * (vert0.y - vert1.y) + y * (vert1.x - vert0.x)));
     }
     else {
-        return new Vec2(0, 0);
+        return new Vec2(1 / determinant * (x * (vert0.y - vert1.y) + y * (vert1.x - vert0.x)), 1 - 1 / determinant * (x * (vert2.y - vert1.y) + y * (vert1.x - vert2.x)));
     }
 }
 function applyPixel(x, y, vert0, vert1, vert2, down, imageData) {
     const uv = getUV(x, y, vert0, vert1, vert2, down);
     const texData = imageData.data;
     const index = (Math.round(uv.y * imageData.height) * imageData.width + Math.round(uv.x * imageData.width)) * 4;
-    if (down) {
-        changePixel(x, y, texData[index], texData[index + 1], texData[index + 2], 255);
-    }
+    changePixel(x, y, texData[index], texData[index + 1], texData[index + 2], 255);
 }
-var test = document.createElement("canvas");
-test.width = testImg.naturalWidth;
-test.height = testImg.naturalHeight;
-//document.body.appendChild(test);
-test.style.borderStyle = "solid";
-test.style.borderWidth = "1px";
-var cont = test.getContext("2d");
-cont.drawImage(testImg, 0, 0);
-const testImageData = cont.getImageData(0, 0, testImg.naturalWidth, testImg.naturalHeight);
-function textureTriangle(vert0, vert1, vert2, down) {
+function textureTriangle(vert0, vert1, vert2, down, textureImageData) {
     var verts = [vert0, vert1, vert2];
     verts.sort((a, b) => a.x - b.x);
     const slope0 = (verts[2].y - verts[0].y) / (verts[2].x - verts[0].x);
@@ -122,7 +120,7 @@ function textureTriangle(vert0, vert1, vert2, down) {
     for (let x = verts[0].x; x < verts[1].x; x++) {
         var yValues = [verts[0].y + Math.floor(delta0), verts[0].y + Math.floor(delta1)].sort((a, b) => a - b);
         for (let y = yValues[0]; y <= yValues[1]; y++) {
-            applyPixel(x, y, vert0, vert1, vert2, down, testImageData);
+            applyPixel(x, y, vert0, vert1, vert2, down, textureImageData);
         }
         delta0 += slope0;
         delta1 += slope1;
@@ -130,7 +128,7 @@ function textureTriangle(vert0, vert1, vert2, down) {
     for (let x = verts[1].x; x <= verts[2].x; x++) {
         var yValues = [verts[0].y + Math.floor(delta0), verts[1].y + Math.floor(delta2)].sort((a, b) => a - b);
         for (let y = yValues[0]; y <= yValues[1]; y++) {
-            applyPixel(x, y, vert0, vert1, vert2, down, testImageData);
+            applyPixel(x, y, vert0, vert1, vert2, down, textureImageData);
         }
         delta0 += slope0;
         delta2 += slope2;
@@ -138,12 +136,17 @@ function textureTriangle(vert0, vert1, vert2, down) {
 }
 function main() {
     canvas.addEventListener("mousedown", event => {
-        debugText.innerText = getUV(event.clientX - rect.left, event.clientY - rect.top, tVec0, tVec1, tVec2, true).toString();
+        const uv = getUV(event.clientX - rect.left, event.clientY - rect.top, tVec0, tVec1, tVec2, false);
+        const index = (Math.round(uv.y * testImageData.height) * testImageData.width + Math.round(uv.x * testImageData.width)) * 4;
+        debugText.innerText = `${uv.toString()}
+        ${Math.round(uv.y * testImageData.height)} ${Math.round(uv.x * testImageData.width)}
+        ${testImageData.data[index]} ${testImageData.data[index + 1]} ${testImageData.data[index + 2]}`;
     });
     drawLine([new Vec2(5, 70), new Vec2(2, 60)]);
     //drawTriangle([new Vec2(50, 50), new Vec2(950, 50), new Vec2(50, 950)])
     //drawTriangle([tVec0, tVec1, tVec2])
-    textureTriangle(tVec0, tVec1, tVec2, true);
+    textureTriangle(new Vec2(50, 800), new Vec2(700, 950), new Vec2(950, 50), true, testImageData);
+    textureTriangle(tVec0, tVec1, tVec2, false, testImageData);
     context.putImageData(imageData, 0, 0);
 }
 main();
--- src/debug.ts
+++ src/debug.ts
@@ -8,20 +8,35 @@ const debugText = document.getElementById("debugText") as HTMLDivElement;
 var imageData = context.createImageData(canvas.width, canvas.height);
 var data = imageData.data;
 
-function changePixel(x: number, y: number, red: number, green: number, blue: number, alpha: number): void {
-    data[(y * imageData.width + x) * 4] = red;
-    data[(y * imageData.width + x) * 4 + 1] = green;
-    data[(y * imageData.width + x) * 4 + 2] = blue;
-    data[(y * imageData.width + x) * 4 + 3] = alpha;
-}
+var test = document.createElement("canvas") as HTMLCanvasElement;
+test.width = testImg.naturalWidth;
+test.height = testImg.naturalHeight;
+//document.body.appendChild(test);
+test.style.borderStyle = "solid";
+test.style.borderWidth = "1px";
+var cont = test.getContext("2d") as CanvasRenderingContext2D;
+cont.drawImage(testImg, 0, 0);
+const testImageData = cont.getImageData(0, 0, testImg.naturalWidth, testImg.naturalHeight);
+
 class Vec2{
     constructor(
         public x: number,
         public y: number
     ){}
     toString(){
-        return `Vec2(x: ${this.x}, y: ${this.y})`
+        return `Vec2(x: ${this.x}, y: ${this.y})`;
     }
+}
+
+const tVec0 = new Vec2(50, 800);
+const tVec1 = new Vec2(200, 500);
+const tVec2 = new Vec2(950, 50);
+
+function changePixel(x: number, y: number, red: number, green: number, blue: number, alpha: number): void {
+    data[(y * imageData.width + x) * 4] = red;
+    data[(y * imageData.width + x) * 4 + 1] = green;
+    data[(y * imageData.width + x) * 4 + 2] = blue;
+    data[(y * imageData.width + x) * 4 + 3] = alpha;
 }
 function drawLine(verts: Vec2[]):void {
     var slope = (verts[1].y - verts[0].y) / (verts[1].x - verts[0].x);
@@ -82,24 +97,23 @@ function drawTriangle(verts: Vec2[]): void{
     }
 }
 
-const tVec0 = new Vec2(50, 800);
-const tVec1 = new Vec2(700, 950);
-const tVec2 = new Vec2(950, 50);
-
 function getUV(x0: number, y0: number, vert0: Vec2, vert1: Vec2, vert2: Vec2, down: boolean): Vec2{
     // vert0 - vert1 = a
     // vert1 - vert2 = b
     // inverse matrix to find a and b in terms of x and y
+    const determinant = (vert1.x - vert0.x) * (vert2.y - vert1.y) - (vert2.x - vert1.x) * (vert1.y - vert0.y);
+    const x = x0 - vert0.x;
+    const y = y0 - vert0.y;
     if(down){
-        const determinant = (vert1.x - vert0.x) * (vert2.y - vert1.y) - (vert2.x - vert1.x) * (vert1.y - vert0.y);
-        const x = x0 - vert0.x;
-        const y = y0 - vert0.y;
         return new Vec2(
             1 / determinant * (x * (vert2.y - vert1.y) + y * (vert1.x - vert2.x)),
-            1 / determinant * (x * (vert0.y - vert1.y) + y * (vert1.x - vert0.x)),
+            1 -  1 / determinant * (x * (vert0.y - vert1.y) + y * (vert1.x - vert0.x))
         );
     } else{
-        return new Vec2(0, 0);
+        return new Vec2(
+            1 / determinant * (x * (vert0.y - vert1.y) + y * (vert1.x - vert0.x)),
+            1 - 1/ determinant * (x * (vert2.y - vert1.y) + y * (vert1.x - vert2.x))
+        );
     }
 }
 
@@ -107,21 +121,11 @@ function applyPixel(x:number, y:number, vert0: Vec2, vert1: Vec2, vert2: Vec2, d
     const uv = getUV(x, y, vert0, vert1, vert2, down)
     const texData = imageData.data;
     const index = (Math.round(uv.y * imageData.height) * imageData.width + Math.round(uv.x * imageData.width)) * 4;
-    if(down){
         changePixel(x, y, texData[index], texData[index + 1], texData[index + 2], 255);
-    }
 }
-var test = document.createElement("canvas") as HTMLCanvasElement;
-test.width = testImg.naturalWidth;
-test.height = testImg.naturalHeight;
-//document.body.appendChild(test);
-test.style.borderStyle = "solid";
-test.style.borderWidth = "1px";
-var cont = test.getContext("2d") as CanvasRenderingContext2D;
-cont.drawImage(testImg, 0, 0);
-const testImageData = cont.getImageData(0, 0, testImg.naturalWidth, testImg.naturalHeight);
 
-function textureTriangle(vert0: Vec2, vert1: Vec2, vert2: Vec2, down: boolean): void {
+
+function textureTriangle(vert0: Vec2, vert1: Vec2, vert2: Vec2, down: boolean, textureImageData:ImageData): void {
     var verts= [vert0, vert1, vert2];
     verts.sort((a, b) => a.x - b.x);
     const slope0 = (verts[2].y - verts[0].y) / (verts[2].x - verts[0].x);
@@ -135,7 +139,7 @@ function textureTriangle(vert0: Vec2, vert1: Vec2, vert2: Vec2, down: boolean): 
     for(let x = verts[0].x; x < verts[1].x; x++){
         var yValues = [verts[0].y + Math.floor(delta0), verts[0].y + Math.floor(delta1)].sort((a, b) => a - b);
         for(let y = yValues[0]; y <= yValues[1]; y++){
-            applyPixel(x, y, vert0, vert1, vert2, down, testImageData);      
+            applyPixel(x, y, vert0, vert1, vert2, down, textureImageData);      
         }
         delta0 += slope0;
         delta1 += slope1;
@@ -143,7 +147,7 @@ function textureTriangle(vert0: Vec2, vert1: Vec2, vert2: Vec2, down: boolean): 
     for(let x = verts[1].x; x <= verts[2].x; x++){
         var yValues = [verts[0].y + Math.floor(delta0), verts[1].y + Math.floor(delta2)].sort((a, b) => a - b);
         for(let y = yValues[0]; y <= yValues[1]; y++){
-            applyPixel(x, y, vert0, vert1, vert2, down, testImageData);
+            applyPixel(x, y, vert0, vert1, vert2, down, textureImageData);
         }
         delta0 += slope0;
         delta2 += slope2;
@@ -152,12 +156,17 @@ function textureTriangle(vert0: Vec2, vert1: Vec2, vert2: Vec2, down: boolean): 
 
 function main(): void {
     canvas.addEventListener("mousedown", event => {
-        debugText.innerText = getUV(event.clientX - rect.left, event.clientY - rect.top, tVec0, tVec1, tVec2, true).toString();
+        const uv = getUV(event.clientX - rect.left, event.clientY - rect.top, tVec0, tVec1, tVec2, false);
+        const index = (Math.round(uv.y * testImageData.height) * testImageData.width + Math.round(uv.x * testImageData.width)) * 4;
+        debugText.innerText = `${uv.toString()}
+        ${Math.round(uv.y * testImageData.height)} ${Math.round(uv.x * testImageData.width)}
+        ${testImageData.data[index]} ${testImageData.data[index + 1]} ${testImageData.data[index + 2]}`;
     })
     drawLine([new Vec2(5, 70), new Vec2(2, 60)]);
     //drawTriangle([new Vec2(50, 50), new Vec2(950, 50), new Vec2(50, 950)])
     //drawTriangle([tVec0, tVec1, tVec2])
-    textureTriangle(tVec0, tVec1, tVec2, true);
+    textureTriangle(new Vec2(50, 800), new Vec2(700, 950), new Vec2(950, 50), true, testImageData);
+    textureTriangle(tVec0, tVec1, tVec2, false, testImageData);
     context.putImageData(imageData, 0, 0,);
 }
 main();
