55e3d2a2ab9574c42e7346e9db7e5bfd4ae7bc00
LpHiX <martin.leung.ht@gmail.com>
2022-08-30 19:20:09 +0800

    WORKING TRIANGLES TEXTRUING

--- src/debug.ts
+++ src/debug.ts
@@ -18,174 +18,11 @@ var cont = test.getContext("2d") as CanvasRenderingContext2D;
 cont.drawImage(testImg, 0, 0);
 const testImageData = cont.getImageData(0, 0, testImg.naturalWidth, testImg.naturalHeight);
 
-class Vec2{
-    constructor(
-        public x: number,
-        public y: number
-    ){}
-    toString(){
-        return `Vec2(${this.x},${this.y})`;
-    }
-}
 class Vec3{
     constructor(
         public x: number,
         public y: number,
-        public z: number
-    ){}
-    toString(){
-        return `Vec2(${this.x},${this.y}, ${this.z})`;
-    }
-}
-
-const tVec0 = new Vec2(50, 800);
-const tVec1 = new Vec2(200, 500);
-const tVec2 = new Vec2(950, 50);
-
-function changePixel(x: number, y: number, red: number, green: number, blue: number, alpha: number): void {
-    data[(y * imageData.width + x) * 4] = red;
-    data[(y * imageData.width + x) * 4 + 1] = green;
-    data[(y * imageData.width + x) * 4 + 2] = blue;
-    data[(y * imageData.width + x) * 4 + 3] = alpha;
-}
-function drawLine(verts: Vec2[]):void {
-    var slope = (verts[1].y - verts[0].y) / (verts[1].x - verts[0].x);
-    if(slope < 1 && slope > -1){
-        if(verts[0].x > verts[1].x){
-            verts = [verts[1], verts[0]];
-        }
-        var deltaY = 0.5;
-        for(let x = verts[0].x; x <= verts[1].x; x++){
-            changePixel(x, verts[0].y + Math.floor(deltaY), 200, 0, 100, 255);
-            deltaY += slope;
-        }
-    } else{
-        if(verts[0].y > verts[1].y){
-            verts = [verts[1], verts[0]];
-        }
-        slope = 1 / slope;
-        var deltaX = 0.5;
-        for(let y = verts[0].y; y <= verts[1].y; y++){
-            changePixel(verts[0].x + Math.floor(deltaX), y, 100, 0, 100, 255);
-            deltaX += slope;
-        }
-    }
-    changePixel(verts[0].x, verts[0].y, 0, 0, 0, 255);
-    changePixel(verts[1].x, verts[1].y, 0, 0, 0, 255);
-}
-
-
-function drawTriangle(verts: Vec2[]): void{
-    verts.sort((a, b) => a.x - b.x);
-    const slope0 = (verts[2].y - verts[0].y) / (verts[2].x - verts[0].x);
-    const slope1 = (verts[1].y - verts[0].y) / (verts[1].x - verts[0].x);
-    const slope2 = (verts[2].y - verts[1].y) / (verts[2].x - verts[1].x);
-    
-    var delta0 = 0.5;
-    var delta1 = 0.5;
-    var delta2 = 0.5;
-
-    for(let x = verts[0].x; x < verts[1].x; x++){
-        var yValues = [verts[0].y + Math.floor(delta0), verts[0].y + Math.floor(delta1)].sort((a, b) => a - b);
-        for(let y = yValues[0]; y <= yValues[1]; y++){
-            changePixel(x, y, 20, 255, 100, 255);      
-        }
-        changePixel(x, verts[0].y + Math.floor(delta0), 20, 255, 100, 255);
-        changePixel(x, verts[0].y + Math.floor(delta1), 20, 0, 255, 255);
-        delta0 += slope0;
-        delta1 += slope1;
-    }
-    for(let x = verts[1].x; x <= verts[2].x; x++){
-        var yValues = [verts[0].y + Math.floor(delta0), verts[1].y + Math.floor(delta2)].sort((a, b) => a - b);
-        for(let y = yValues[0]; y <= yValues[1]; y++){
-            changePixel(x, y, y, 255, x, 255);      
-        }
-        changePixel(x, verts[0].y + Math.floor(delta0), 20, 255, 100, 255);
-        changePixel(x, verts[1].y + Math.floor(delta2), 20, 0, 255, 255);
-        delta0 += slope0;
-        delta2 += slope2;
-    }
-}
-
-function getUV(x0: number, y0: number, vert0: Vec2, vert1: Vec2, vert2: Vec2, down: boolean): Vec2{
-    // vert0 - vert1 = a
-    // vert1 - vert2 = b
-    // inverse matrix to find a and b in terms of x and y
-    const determinant = (vert1.x - vert0.x) * (vert2.y - vert1.y) - (vert2.x - vert1.x) * (vert1.y - vert0.y);
-    const x = x0 - vert0.x;
-    const y = y0 - vert0.y;
-    if(down){
-        return new Vec2(
-            1 / determinant * (x * (vert2.y - vert1.y) + y * (vert1.x - vert2.x)),
-            1 -  1 / determinant * (x * (vert0.y - vert1.y) + y * (vert1.x - vert0.x))
-        );
-    } else{
-        return new Vec2(
-            1 / determinant * (x * (vert0.y - vert1.y) + y * (vert1.x - vert0.x)),
-            1 - 1/ determinant * (x * (vert2.y - vert1.y) + y * (vert1.x - vert2.x))
-        );
-    }
-}
-
-function applyPixel(x:number, y:number, vert0: Vec2, vert1: Vec2, vert2: Vec2, down: boolean, imageData: ImageData){
-    const uv = getUV(x, y, vert0, vert1, vert2, down)
-    const texData = imageData.data;
-    const index = (Math.round(uv.y * imageData.height) * imageData.width + Math.round(uv.x * imageData.width)) * 4;
-        changePixel(x, y, texData[index], texData[index + 1], texData[index + 2], 255);
-}
-
-
-function textureTriangle(vert0: Vec2, vert1: Vec2, vert2: Vec2, down: boolean, textureImageData:ImageData): void {
-    var verts= [vert0, vert1, vert2];
-    verts.sort((a, b) => a.x - b.x);
-    const slope0 = (verts[2].y - verts[0].y) / (verts[2].x - verts[0].x);
-    const slope1 = (verts[1].y - verts[0].y) / (verts[1].x - verts[0].x);
-    const slope2 = (verts[2].y - verts[1].y) / (verts[2].x - verts[1].x);
-    
-    var delta0 = 0.5;
-    var delta1 = 0.5;
-    var delta2 = 0.5;
-
-    for(let x = verts[0].x; x < verts[1].x; x++){
-        var yValues = [verts[0].y + Math.floor(delta0), verts[0].y + Math.floor(delta1)].sort((a, b) => a - b);
-        for(let y = yValues[0]; y <= yValues[1]; y++){
-            applyPixel(x, y, vert0, vert1, vert2, down, textureImageData);      
-        }
-        delta0 += slope0;
-        delta1 += slope1;
-    }
-    for(let x = verts[1].x; x <= verts[2].x; x++){
-        var yValues = [verts[0].y + Math.floor(delta0), verts[1].y + Math.floor(delta2)].sort((a, b) => a - b);
-        for(let y = yValues[0]; y <= yValues[1]; y++){
-            applyPixel(x, y, vert0, vert1, vert2, down, textureImageData);
-        }
-        delta0 += slope0;
-        delta2 += slope2;
-    }
-}
-
-function main(): void {
-    canvas.addEventListener("mousedown", event => {
-        const uv = getUV(event.clientX - rect.left, event.clientY - rect.top, tVec0, tVec1, tVec2, false);
-        const index = (Math.round(uv.y * testImageData.height) * testImageData.width + Math.round(uv.x * testImageData.width)) * 4;
-        debugText.innerText = `${uv.toString()}
-        ${Math.round(uv.y * testImageData.height)} ${Math.round(uv.x * testImageData.width)}
-        ${testImageData.data[index]} ${testImageData.data[index + 1]} ${testImageData.data[index + 2]}`;
-    })
-    drawLine([new Vec2(5, 70), new Vec2(2, 60)]);
-    //drawTriangle([new Vec2(50, 50), new Vec2(950, 50), new Vec2(50, 950)])
-    //drawTriangle([tVec0, tVec1, tVec2])
-    textureTriangle(new Vec2(50, 800), new Vec2(700, 950), new Vec2(950, 50), true, testImageData);
-    textureTriangle(tVec0, tVec1, tVec2, false, testImageData);
-    context.putImageData(imageData, 0, 0,);
-}
-main();
-*/
-class Vec3{
-    constructor(
-        public x: number,
-        public y: number,
-        public z: number
+        public z: number,
     ){}
     toString(){
         return `Vec3(${this.x}, ${this.y}, ${this.z})`;
@@ -200,6 +37,12 @@ class Vec3{
         return a.add(b.add(a.mul(-1)).mul(alpha));
     }
 }
+class Vertex{
+    constructor(
+        public pos: Vec3,
+        public uv: Vec3
+    ){}
+}
 class Grid{
     constructor(
         public size: number
@@ -210,8 +53,8 @@ class Grid{
     canvasY(vec: Vec3):number{
         return (1 - vec.y / this.size) * canvas.height / 2;
     }
-    canvasVec(vec: Vec3):Vec3{
-        return new Vec3(this.canvasX(vec), this.canvasY(vec), vec.z);
+    canvasVec(vert: Vertex):Vertex{
+        return new Vertex(new Vec3(this.canvasX(vert.pos), this.canvasY(vert.pos), vert.pos.z), vert.uv);
     }
 }
 function changePixel(vec: Vec3, color: Vec3){
@@ -222,105 +65,65 @@ function changePixel(vec: Vec3, color: Vec3){
     data[index + 3] = 255;
 }
 const grid = new Grid(1);
-// Takes verts in form u/z, v/z, 1/z
-function getAB(x0: number, y0: number, vert0: Vec3, vert1: Vec3, vert2: Vec3, down: boolean){
-    // vert0 - vert1 = a
-    // vert1 - vert2 = b
-    // inverse matrix to find a and b in terms of x and y
-    vert0 = grid.canvasVec(vert0);
-    vert1 = grid.canvasVec(vert1);
-    vert2 = grid.canvasVec(vert2);
 
-    const determinant = (vert1.x - vert0.x) * (vert2.y - vert1.y) - (vert2.x - vert1.x) * (vert1.y - vert0.y);
-    const x = x0 - vert0.x;
-    const y = y0 - vert0.y;
-    if(down){
-        return new Vec3(
-            1 / determinant * (x * (vert2.y - vert1.y) + y * (vert1.x - vert2.x)),
-            1 - 1/ determinant * (x * (vert0.y - vert1.y) + y * (vert1.x - vert0.x)), 0
-        );
-    } else{
-        return new Vec3(
-            1 / determinant * (x * (vert0.y - vert1.y) + y * (vert1.x - vert0.x)),
-            1 - 1/ determinant * (x * (vert2.y - vert1.y) + y * (vert1.x - vert2.x)), 0
-        );
-    }
-}
-// Takes verts in form u/z, v/z, 1/z
-function getColor(uv: Vec3, down: boolean, texture: ImageData): Vec3{
+function getColor(uv: Vec3, texture: ImageData): Vec3{
 
-    const index = (Math.round(uv.y  / 1 * texture.height) * texture.width + Math.round(uv.x / 1 * texture.width)) * 4;
+    const index = (Math.round(uv.y  / uv.z * texture.height) * texture.width + Math.round(uv.x / uv.z * texture.width)) * 4;
     return new Vec3(texture.data[index], texture.data[index + 1], texture.data[index + 2]);
 }
-function drawTriangle(vert0: Vec3, vert1: Vec3, vert2: Vec3, down: boolean, texture: ImageData){
-    vert0 = new Vec3(vert0.x / vert0.z, vert0.y / vert0.z, 1 / vert0.z);
-    vert1 = new Vec3(vert1.x / vert1.z, vert1.y / vert1.z, 1 / vert1.z);
-    vert2 = new Vec3(vert2.x / vert2.z, vert2.y / vert2.z, 1 / vert2.z);
-    const uv0 = new Vec3(1, 0, 1).mul(1);
-    const uv1 = new Vec3(0, 1, 1).mul(1);
-    const uv2 = new Vec3(1, 1, 1).mul(1);
-    var rawVerts = [vert0, vert1, vert2].sort((a, b) => b.y - a.y);
-    var verts = [grid.canvasVec(rawVerts[0]), grid.canvasVec(rawVerts[1]), grid.canvasVec(rawVerts[2])]
+function drawTriangle(vert0: Vertex, vert1: Vertex, vert2: Vertex, texture: ImageData){
+    vert0.uv = vert0.uv.mul(1/vert0.pos.z);
+    vert1.uv = vert1.uv.mul(1/vert1.pos.z);
+    vert2.uv = vert2.uv.mul(1/vert2.pos.z);
+    vert0.pos = vert0.pos.mul(1/vert0.pos.z);
+    vert1.pos = vert1.pos.mul(1/vert1.pos.z);
+    vert2.pos = vert2.pos.mul(1/vert2.pos.z);
+    var rawVerts = [vert0, vert1, vert2].sort((a, b) => b.pos.y - a.pos.y);
+    var verts = [grid.canvasVec(rawVerts[0]).pos, grid.canvasVec(rawVerts[1]).pos, grid.canvasVec(rawVerts[2]).pos]
     const dLine0 = verts[2].add(verts[0].mul(-1))
     const dLine1 = verts[1].add(verts[0].mul(-1))
     const dLine2 = verts[2].add(verts[1].mul(-1))
-    const dUV0 = uv2.add(uv0.mul(-1));
-    const dUV1 = uv1.add(uv0.mul(-1));
-    const dUV2 = uv2.add(uv1.mul(-1));
     var delta0 = 0.5;
     var delta1 = 0.5;
     var delta2 = 0.5;
     console.log(verts)
     for(let y = Math.round(verts[0].y); y < Math.round(verts[1].y); y++){
         let xValues = [Math.round(verts[0].x) + Math.floor(delta0), Math.round(verts[0].x) + Math.floor(delta1)];
-        let lineUV = [Vec3.lerp((y - verts[0].y) / (verts[2].y - verts[0].y), uv0, uv1), Vec3.lerp((y - verts[0].y) / (verts[1].y - verts[0].y), uv0, uv2)]
+        let lineUV = [Vec3.lerp((y - verts[0].y) / (verts[2].y - verts[0].y), rawVerts[0].uv, rawVerts[2].uv), Vec3.lerp((y - verts[0].y) / (verts[1].y - verts[0].y), rawVerts[0].uv, rawVerts[1].uv)]
         if(xValues[0] > xValues[1]){
             xValues = [xValues[1], xValues[0]];
             lineUV = [lineUV[1], lineUV[0]];
         }
         for(let x = xValues[0]; x <= xValues[1]; x++){
             const uv = Vec3.lerp((x - xValues[0])/(xValues[1] - xValues[0]), lineUV[0], lineUV[1]);
-            changePixel(new Vec3(x, y, 0), getColor(uv, down, testImageData));
+            changePixel(new Vec3(x, y, 0), getColor(uv, testImageData));
         }
         delta0 += dLine0.x / dLine0.y
         delta1 += dLine1.x / dLine1.y
     }
     for(let y = Math.round(verts[1].y); y < Math.round(verts[2].y); y++){
         let xValues = [Math.round(verts[0].x) + Math.floor(delta0), Math.round(verts[1].x) + Math.floor(delta2)];
-        let lineUV = [Vec3.lerp((y - verts[0].y) / (verts[2].y - verts[0].y), uv0, uv2), Vec3.lerp((y - verts[1].y) / (verts[2].y - verts[1].y), uv1, uv2)]
+        let lineUV = [Vec3.lerp((y - verts[0].y) / (verts[2].y - verts[0].y), rawVerts[0].uv, rawVerts[2].uv), Vec3.lerp((y - verts[1].y) / (verts[2].y - verts[1].y), rawVerts[1].uv, rawVerts[2].uv)]
         if(xValues[0] > xValues[1]){
             xValues = [xValues[1], xValues[0]];
             lineUV = [lineUV[1], lineUV[0]];
         }
         for(let x = xValues[0]; x <= xValues[1]; x++){
             const uv = Vec3.lerp((x - xValues[0])/(xValues[1] - xValues[0]), lineUV[0], lineUV[1]);
-            changePixel(new Vec3(x, y, 0), getColor(uv, down, testImageData));
+            changePixel(new Vec3(x, y, 0), getColor(uv, testImageData));
         }
         delta0 += dLine0.x / dLine0.y
         delta2 += dLine2.x / dLine2.y
     }
 }
-function divZ(vec:Vec3):Vec3{
-    return new Vec3(vec.x / vec.z, vec.y / vec.z, 1 / vec.z);
-}
 function main(){
-    const tVec0 = new Vec3(-10, -10, 25)
-    const tVec1 = new Vec3(10, -10, 15)
-    const tVec2 = new Vec3(10, 10, 12)
+    const tVert0 = new Vertex(new Vec3(-10, 10, 25),new Vec3(0, 0, 1))
+    const tVert1 = new Vertex(new Vec3(10, 10, 15) ,new Vec3(1, 0, 1))
+    const tVert2 = new Vertex(new Vec3(10, -10, 12)  ,new Vec3(1, 1, 1))
     canvas.addEventListener("mousedown", event =>{
-        const ab = getAB(event.clientX - rect.left, event.clientY - rect.top, divZ(tVec0), divZ(tVec1), divZ(tVec2), true);
-        const u = Vec3.lerp(ab.x, new Vec3(0, 0, divZ(tVec0).z), new Vec3(1 * divZ(tVec1).z, 0, divZ(tVec1).z));
-        const v = Vec3.lerp(ab.y, new Vec3(0, 0, divZ(tVec1).z), new Vec3(0, 1 * divZ(tVec2).z, divZ(tVec2).z));
-        const index = (Math.round(u.x / u.z * testImageData.height) * testImageData.width   + Math.round(v.y / v.z * testImageData.width)) * 4;
-        const color = new Vec3(testImageData.data[index], testImageData.data[index + 1], testImageData.data[index + 2]);
-        debugText.innerText = 
-        `AB = ${ab.toString()}
-        UV div= (${u.x}, ${v.y})
-        UV = (${u.x / u.z}, ${v.y / v.z})
-        tex = (${Math.round(u.x / u.z * testImageData.height) }, ${Math.round(v.y / v.z * testImageData.width) }
-        color = ${color.toString()}`
     })
-    drawTriangle(tVec0, tVec1, tVec2, true, testImageData);
+    drawTriangle(tVert0, tVert1, tVert2, testImageData);
+    drawTriangle(tVert0, new Vertex(new Vec3(-10, -10, 20), new Vec3(0, 1, 1)), tVert2, testImageData);
     //drawTriangle(tVec0, new Vec3(0, 10, 30), tVec2, false, testImageData);
     context.putImageData(imageData, 0, 0);
 }
