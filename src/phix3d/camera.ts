3ee4af17f2f63a323f172b2a56b5102af0b33dd8
LpHiX <erpastian@gmail.com>
2022-09-03 00:39:55 +0800

    starting

--- src/phix3d/camera.ts
+++ src/phix3d/camera.ts
@@ -1,16 +1,46 @@
+import { World } from "./scene3d";
 import { Vec3 } from "./structs.js";
 
+export class FrustPlane{
+    constructor(
+        public normal: Vec3,
+        public distance: number
+    ){}
+}
+
 export class Camera{
+    planes: FrustPlane[];
     constructor(
         public pos: Vec3,
         public eulerRot: Vec3,
-        public frustumSize: Vec3,
+        public sizeX: number,
+        public sizeY: number,
         public near: number
-    ){}
+    ){
+        const angleX = Math.atan2(sizeX, 1);
+        const angleY = Math.atan2(sizeY, 1);
+        const normHX = Math.cos(angleX);
+        const normHZ = Math.sin(angleX);
+        const normVY = Math.cos(angleY);
+        const normVZ = Math.sin(angleY);
+        this.planes = [
+            new FrustPlane(new Vec3(normHX, 0, normHZ), 0),
+            new FrustPlane(new Vec3(-normHX, 0, normHZ), 0),
+            new FrustPlane(new Vec3(0, normVY, normVZ), 0),
+            new FrustPlane(new Vec3(0, -normVY, normVZ), 0),
+            new FrustPlane(new Vec3(0, 0, 1), near)
+        ]
+    }
 }
 export class Viewport{
+    ctx: CanvasRenderingContext2D;
     constructor(
         public canvas: HTMLCanvasElement,
         public camera: Camera,
-    ){}
+    ){
+        this.ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    }
+    drawWorld(world: World){
+        this.ctx.createImageData(10, 10).data
+    }
 }
--- src/phix3d/index.ts
+++ src/phix3d/index.ts
@@ -1,7 +1,9 @@
+import { Camera, Viewport } from "./camera.js";
 import { Vec3 } from "./structs.js";
 
-const test = new Vec3(0, 0, 0);
-const
+const canvas0 = document.getElementById("canvas") as HTMLCanvasElement;
+const cam0 = new Camera(new Vec3(0, 0, 0), new Vec3(0, 0, 0), 1, 1, 0.1);
+const viewport0 = new Viewport(canvas0, cam0);
 
 function setup(){
 
@@ -9,6 +11,7 @@ function setup(){
 
 function mainLoop(timestamp: number){
 
+    requestAnimationFrame(mainLoop);
 }
 
 setup();
